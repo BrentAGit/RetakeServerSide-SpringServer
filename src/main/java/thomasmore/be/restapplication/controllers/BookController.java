d80405a862885002c8bb31ec4d403e2e7083f1d5
BrentAGit <r0787709@student.thomasmore.be>
2021-06-09 00:03:32 +0200

    Added logger + Postmapping

--- src/main/java/thomasmore/be/restapplication/controllers/BookController.java
+++ src/main/java/thomasmore/be/restapplication/controllers/BookController.java
@@ -1,9 +1,9 @@
 package thomasmore.be.restapplication.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import thomasmore.be.restapplication.model.Book;
 import thomasmore.be.restapplication.repositories.BookRepository;
 
@@ -15,9 +15,19 @@ public class BookController {
     @Autowired
     private BookRepository bookRepository;
 
+    Logger logger = LoggerFactory.getLogger(BookController.class);
+
     @CrossOrigin
     @GetMapping("/books")
     private List<Book> findAll() {
+        logger.info("findAll");
         return bookRepository.findAll();
+    }
+
+    @CrossOrigin
+    @PostMapping("/books")
+    public Book create(@RequestBody Book book){
+        logger.info("create");
+        return bookRepository.save(book);
     }
 }
