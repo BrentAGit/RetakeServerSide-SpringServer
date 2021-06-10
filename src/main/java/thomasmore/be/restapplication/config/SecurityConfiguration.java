62eb1bc481693113a505950278a5839c4dc501e3
BrentAGit <r0787709@student.thomasmore.be>
2021-06-10 22:22:14 +0200

    Added security configuration and csrf token sender

---
+++ src/main/java/thomasmore/be/restapplication/config/SecurityConfiguration.java
@@ -0,0 +1,19 @@
+package thomasmore.be.restapplication.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        http.authorizeRequests().anyRequest().authenticated();
+        http.httpBasic();
+        //http.formLogin();
+        http.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()); //Stuurt csrf token naar front-end
+    }
+}
